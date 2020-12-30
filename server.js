57944ee99870fd6e3a8f78e284b506d4401a5424
Andre Machado <soutomachado1@gmail.com>
2020-12-30 17:05:38 -0300

    Testando o start

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors())
 app.use('/', () => resolve(__dirname, './build'));
 
+app.use('/favicon.ico', (req, res) => res.send());
+
 app.listen(PORT, (err) => {
   if(err){
     return console.log(err);
