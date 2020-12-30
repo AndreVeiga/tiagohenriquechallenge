5acfd727bc9bfbda99951d33de193f9fe5855625
Andre Machado <soutomachado1@gmail.com>
2020-12-30 16:52:23 -0300

    Testando o start

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from './components/menu'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import React from 'react';
 
 export default function App() {
-  const classes = useStyles();
-
-  const [openMenu, setOpenMenu] = useState(false);
-
-  const renderMenu = () => {
-    if(openMenu) {
-      return (
-        <Menu
-          open={openMenu}
-          setOpenMenu={setOpenMenu} />
-      );
-    }
-  }
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton}
-            color="inherit" aria-label="menu"
-            onClick={() => setOpenMenu(!openMenu)}>
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-
-      { renderMenu() }
+    <div>
+      <h1>Teste</h1>
     </div>
   );
 }
--- src/components/menu/itemMenu.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppsIcon from '@material-ui/icons/Apps';
-
-const items = [
-  { text: 'Desafios', icon: <AppsIcon /> }
-];
-
-export default items;
